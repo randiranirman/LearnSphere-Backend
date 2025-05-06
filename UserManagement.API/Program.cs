a5aeb5d9f58524d79fffea6929a5952ba4634c5b
Randira nirman <randiranirman20011210@gmail.com>
2025-05-06 15:13:27 +0530

    added the login funcatilaity and logout functionlatity

--- UserManagement.API/Program.cs
+++ UserManagement.API/Program.cs
@@ -1,3 +1,6 @@
+using UserManagement.Domain.Repositories;
+using UserManagement.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAuthService, AuthService>();
+ 
+builder.Services.AddAuthentication( JwtBearerDefaults.AuthenticationScheme )
 
 var app = builder.Build();
 
