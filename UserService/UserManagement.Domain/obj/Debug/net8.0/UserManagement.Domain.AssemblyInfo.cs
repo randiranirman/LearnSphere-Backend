694dfaf600ebe818aedc7b87394c480ca96004cc
randiranirman <randiranirman20011210@gmail.com>
2025-06-07 22:04:20 +0530

    update the files

--- UserService/UserManagement.Domain/obj/Debug/net8.0/UserManagement.Domain.AssemblyInfo.cs
+++ UserService/UserManagement.Domain/obj/Debug/net8.0/UserManagement.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a63f18c31137b20724668f6f1b06fc4e633e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8db3879ddf12cf58922fb381c8bae18a659a923")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
