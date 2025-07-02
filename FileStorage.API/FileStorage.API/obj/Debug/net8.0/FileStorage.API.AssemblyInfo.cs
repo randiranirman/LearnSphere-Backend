ccd661538ce951951dccd5a452dd4a4ed31e2d15
ChanukaDasun <chanuka101@gmail.com>
2025-07-02 21:43:24 +0530

    save changes from unsaved

--- FileStorage.API/FileStorage.API/obj/Debug/net8.0/FileStorage.API.AssemblyInfo.cs
+++ FileStorage.API/FileStorage.API/obj/Debug/net8.0/FileStorage.API.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileStorage.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdc511f1f103929e03b8d6f58b508e4a02e8d0f")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5fd9990988aaffe2722392b9f41ad3adf670ff")]
->>>>>>> 19d824cb93964ee44447f33d97e0ca0d6f742642
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9bb5f8074bc77c6f5149d0221a1c560be6cf169")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileStorage.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileStorage.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FileStorage.API/FileStorage.Domain/obj/Debug/net8.0/FileStorage.Domain.AssemblyInfo.cs
+++ FileStorage.API/FileStorage.Domain/obj/Debug/net8.0/FileStorage.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileStorage.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5fd9990988aaffe2722392b9f41ad3adf670ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9bb5f8074bc77c6f5149d0221a1c560be6cf169")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileStorage.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileStorage.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
