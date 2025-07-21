aab0ade2b63603cc9ff112a5c0ca8225d0218d2e
ChanukaDasun <chanuka101@gmail.com>
2025-07-21 12:58:21 +0530

    save changes from resolving conflicts

--- QuizManagementService/QuizManagement.API/obj/Debug/net8.0/QuizManagement.API.AssemblyInfo.cs
+++ QuizManagementService/QuizManagement.API/obj/Debug/net8.0/QuizManagement.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e143493f27fae5ae0243418618039b5db14352d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2195eb315ef5a7ab536c70732801acb87352110d")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
